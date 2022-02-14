d696537413b62fefea4f9c670190052163434198
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-02-14 14:49:00 +0000

    extras

--- 03_Intermediate/01_lists.py
+++ 03_Intermediate/01_lists.py
@@ -31,3 +31,48 @@ mylist.insert(1, 'Blueberry')
 
 # Remove item
 lastItem = mylist.pop()
+
+# remove specific item
+mylist.remove("apple")
+
+# reverse the list
+mylist.reverse()
+
+# sort
+numbers = [1,4,3,6,8,2,5,11,333]
+
+numbers.sort() # mutates data
+
+new_list = sorted(numbers) # does not mutate original list
+
+#  repeats elements in list
+multiplied_list = [0] * 5 
+print(multiplied_list)
+
+# SLICING - [start index : end index] 
+
+sliced = [1,2,3,4,5,6,7,8,9]
+
+a = sliced[1:5]
+
+print(a)
+
+# 
+
+b = sliced[::2] # only includes every 2 items
+
+print(b)
+
+# when copying use append. if you edit an assigned list the original gets editted too
+
+newList = sliced
+
+print(sliced)
+newList.pop() # Also edits sliced list
+print(sliced)
+
+# list comprehension!
+
+newly_sliced = [x * x for x in sliced]
+
+print(newly_sliced)
